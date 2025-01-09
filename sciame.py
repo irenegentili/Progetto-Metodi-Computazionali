f522c40216895e48ef706e540f51e6fa0530f54e
irenegentili <irene_gentili@PC-DI-IRENE.>
2025-01-09 18:12:45 +0100

    aggiunte modifiche

--- sciame.py
+++ sciame.py
@@ -4,11 +4,11 @@ import pandas as pd
 #definizione costanti usate 
 
 X0 = 7e4 # lunghezza di radiazione (cm)
-quotainiz = 2e6 # quota iniziale sciame (cm)
-quotariv = 4e5 # quota rivelatore (cm)
+quotainiz = 2.0e6 # quota iniziale sciame (cm)
+quotariv = 4.0e5 # quota rivelatore (cm)
 Ec_elet = 87.92 # energia critica elettroni (MeV)
 Ec_pos = 85.97 # energia critica positroni (MeV)
-c = 3e10 # (cm/s)
+c = 3.0e10 # (cm/s)
 Er= 0.51 #( energia a riposo elettrone e positrone MeV)
 en_coppia = 2*Er # energia legata alla produzione di coppie (MeV)
 min_ioniz = 2.187e-3 # minimo di ionizzazione (MeV/cm)
@@ -51,7 +51,7 @@ def perdita_ionizzazione(index, df, s):
     df: dataframe con le particelle dello sciame
     s: passo di avanzamento della simulazione in frazioni di X0 (compreso tra 0 e 1)
     """
-    df.loc[index, 'energia'] = float(df.loc[index, 'energia']) - min_ioniz * s * X0
+    df.loc[index, 'energia'] = df.loc[index, 'energia'] - min_ioniz * s * X0
     return df
 
 def aggiungi_part(df, dict):
@@ -103,8 +103,7 @@ def simulazione_sciame(E0, s=0.1, angolo=0):
         #creo una lista per memorizzare le particelle nuove ottenute dai vari processi
         new_particelle= [] 
 
-        #creo una lista di particelle da eliminare
-        drop_particelle=[]
+        sciame = sciame.reset_index(drop=True)
 
         for index, particella in sciame.iterrows():
             E = particella['energia']
@@ -121,11 +120,10 @@ def simulazione_sciame(E0, s=0.1, angolo=0):
                 if E > min_ioniz*s*X0 :
                     sciame= perdita_ionizzazione(index, sciame, s)
                 else:
-                    #print(f"Elimino {tipo} con energia {E:.2f} MeV perchè E<E_min *s*X0 con E_min pari all'energia di Minimum ionization (MeV/cm) ")
-                    drop_particelle.append(index)
+                    sciame.loc[index, 'energia'] = 0
 
                 if np.random.uniform() < prob_brems(s) and E>Ec:
-                    sciame.loc[index, 'energia'] = float(sciame.loc[index, 'energia'])/2
+                    sciame.loc[index, 'energia'] = sciame.loc[index, 'energia']/2
                     new_particelle.append({'tipo': 'fotone', 'energia': E/2})
 
 
@@ -133,17 +131,18 @@ def simulazione_sciame(E0, s=0.1, angolo=0):
             if tipo == 'fotone':
                 if E > en_coppia:
                     if np.random.uniform() < prob_coppia(s):
-                        drop_particelle.append(index)
-                        new_particelle.append({'tipo': 'positrone', 'energia': float(E)/2})
-                        new_particelle.append({'tipo': 'elettrone', 'energia': float(E)/2})
+                        sciame.loc[index, 'energia'] = 0 
+                        new_particelle.append({'tipo': 'positrone', 'energia': E/2})
+                        new_particelle.append({'tipo': 'elettrone', 'energia': E/2})
                 else:
-                    #print(f"Elimino {tipo} con energia {E:.2f} MeV perchè  E < 2*m_e *c^2")
-                    drop_particelle.append(index)
-        sciame = sciame.drop(drop_particelle, axis=0)
+                    sciame.loc[index, 'energia'] = 0 
+
+        sciame = sciame.loc[sciame['energia'] > 0]
+
         if new_particelle:
             sciame= aggiungi_part(sciame, new_particelle)
         
-        quota=float(quota) - spost_vert
+        quota-= spost_vert
 
     particelle_rivelate = len(sciame)
 
