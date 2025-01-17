f7a860e5b56bdcea90ec6fc553a325aba28be90a
irenegentili <irene_gentili@PC-DI-IRENE.>
2025-01-17 20:49:42 +0100

    aggiunte modifiche

--- sciame.py
+++ sciame.py
@@ -9,7 +9,7 @@ quotariv = 4.0e5 # quota rivelatore (cm)
 Ec_elet = 87.92 # energia critica elettroni (MeV)
 Ec_pos = 85.97 # energia critica positroni (MeV)
 c = 3.0e10 # (cm/s)
-Er= 0.51 #( energia a riposo elettrone e positrone MeV)
+Er= 0.511 #energia a riposo elettrone e positrone (MeV)
 en_coppia = 2*Er # energia legata alla produzione di coppie (MeV)
 min_ioniz = 2.187e-3 # minimo di ionizzazione (MeV/cm)
 
@@ -43,27 +43,28 @@ def prob_coppia(s):
     
     return probabilita
 
-def perdita_ionizzazione(index, df, s):
-    """
-    funzione perdita_ionizzazione(s,E) per determinare l'energia residua 
-    dopo il processo di ionizzazione di e+ o e-
-    index: indice della particella nel dataframe
-    df: dataframe con le particelle dello sciame
-    s: passo di avanzamento della simulazione in frazioni di X0 (compreso tra 0 e 1)
-    """
-    df.loc[index, 'energia'] = df.loc[index, 'energia'] - min_ioniz * s * X0
-    return df
 
-def aggiungi_part(df, dict):
+def aggiungi_part(df, lista):
     """
     funzione per aggiungere una nuova particella al dataframe dello sciame
     df: dataframe
-    dict: lista di dizionari contenente le nuove particelle
+    lista: lista di tuple con dati delle nuove particelle
 
     return dataframe con le particelle aggiornate
     """
-    new_df = pd.DataFrame(dict)  # Creo un DataFrame dalla lista di dizionari
-    return pd.merge(df, new_df, how='outer')
+    #se la lista è vuota restituisce df
+    if not lista: 
+        return df
+    
+    #creo un dataframe a partire dalla lista
+    df_new = pd.DataFrame(lista, columns=['tipo', 'energia'])
+
+    df_new = df_new.dropna(how='all') #elimino le righe contenenti NaN
+    df_new = df_new.dropna(axis=1, how='all') #elimino le colonne contenenti NaN
+    df = df.dropna(how='all') #elimino le righe contenenti NaN
+    df = df.dropna(axis=1, how='all') #elimino le colonne contenenti NaN
+
+    return pd.concat([df, df_new], ignore_index=True)
 
 def genera_particella_iniziale(E0):
     """
@@ -75,7 +76,7 @@ def genera_particella_iniziale(E0):
     return particella: un dizionario dove si specifica il tipo e l'energia della particella iniziale
     """
     tipo= np.random.choice(['elettrone', 'positrone', 'fotone'])
-    particella = {'tipo':tipo,'energia': E0}
+    particella = (tipo, E0)
     return particella
 
 
@@ -97,17 +98,21 @@ def simulazione_sciame(E0, s=0.1, angolo=0):
     sciame = pd.DataFrame( columns=['tipo', 'energia']) #creo il df contenente lo sciame
     primapart = genera_particella_iniziale(E0) #genero casualmente la particella iniziale
     sciame= aggiungi_part(sciame, [primapart]) #aggiungo la prima particella allo sciame
-
+    
     while quota > quotariv and len(sciame)!=0:
 
-        #creo una lista per memorizzare le particelle nuove ottenute dai vari processi
-        new_particelle= [] 
+        #inizializzo una lista per memorizzare le tuple contenenti le particelle nuove ottenute dai vari processi
+        new_particelle = []
 
+        #resetto l'indice per sicurezza all'inizio di ogni iterazione del ciclo while
         sciame = sciame.reset_index(drop=True)
-
-        for index, particella in sciame.iterrows():
-            E = particella['energia']
-            tipo = particella['tipo']
+        #creo una lista per memorizzare le tuple (indice,nuovi valori energie delle particelle)
+        new_energie= [] 
+        #ciclo for sulle righe del dataframe sciame
+        for particella in sciame.itertuples():
+            index = particella.Index  # Indice della riga
+            energy = particella.energia  # Energia della particella
+            tipo = particella.tipo   # Tipo della particella
 
             if tipo in ['elettrone', 'positrone']:                
                 if tipo =='elettrone':
@@ -116,34 +121,45 @@ def simulazione_sciame(E0, s=0.1, angolo=0):
                 else:
                     #associo la corrispondente energia critica
                     Ec=Ec_pos
-
-                if E > min_ioniz*s*X0 :
-                    sciame= perdita_ionizzazione(index, sciame, s)
+                
+                if energy > min_ioniz*s*X0 :
+                    en_dimin= energy - min_ioniz * s * X0
+                    E_fin = en_dimin #introduco questa variabile per tenere conto dei cambiamenti dell'energia
                 else:
-                    sciame.loc[index, 'energia'] = 0
+                    #pongo l'energia=0 così da usare questa condizione per eliminare la particella alla fine
+                    E_fin=0
+                    
 
-                if np.random.uniform() < prob_brems(s) and E>Ec:
-                    sciame.loc[index, 'energia'] = sciame.loc[index, 'energia']/2
-                    new_particelle.append({'tipo': 'fotone', 'energia': E/2})
+                if np.random.uniform() < prob_brems(s) and E_fin>Ec:
+                    E_fin=E_fin/2
+                    new_particelle.append(("fotone", E_fin))
 
-
+                new_energie.append((index, E_fin))
             
             if tipo == 'fotone':
-                if E > en_coppia:
+                if energy > en_coppia:
                     if np.random.uniform() < prob_coppia(s):
-                        sciame.loc[index, 'energia'] = 0 
-                        new_particelle.append({'tipo': 'positrone', 'energia': E/2})
-                        new_particelle.append({'tipo': 'elettrone', 'energia': E/2})
+                        #pongo l'energia=0 così da usare questa condizione per eliminare la particella alla fine
+                        new_energie.append((index, 0))
+                        new_particelle.append(("elettrone", energy/2)) 
+                        new_particelle.append(("positrone", energy/2))     
                 else:
-                    sciame.loc[index, 'energia'] = 0 
-
-        sciame = sciame.loc[sciame['energia'] > 0]
-
-        if new_particelle:
-            sciame= aggiungi_part(sciame, new_particelle)
+                    new_energie.append((index, 0))
         
+        #associo i nuovi valori delle energie
+        for i, en in new_energie:
+            sciame.at[i, 'energia'] = en
+
+        #elimino le particelle con energia=0
+        sciame.drop(sciame[sciame['energia'] == 0].index, inplace=True)
+
+        #aggiungo le nuove particelle
+        sciame= aggiungi_part(sciame, new_particelle)
+        
+        #diminuisco la quota 
         quota-= spost_vert
 
+    #determino il numero delle particelle rivelate
     particelle_rivelate = len(sciame)
 
     return particelle_rivelate
